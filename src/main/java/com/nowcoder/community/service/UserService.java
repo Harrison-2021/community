d791be823a68dd618b9dcc07db5aadc9fb00ddb9
liusheng <liushenggcs@163.com>
2021-09-18 23:29:30 +0800

    2.2.3_验证邮箱，用邮箱中接收的url激活用户

--- src/main/java/com/nowcoder/community/service/UserService.java
+++ src/main/java/com/nowcoder/community/service/UserService.java
@@ -1,6 +1,7 @@
 package com.nowcoder.community.service;
 
 import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.CommunityConstant;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.util.CommunityUtil;
 import com.nowcoder.community.util.MailClient;
@@ -17,7 +18,7 @@ import java.util.Map;
 import java.util.Random;
 
 @Service
-public class UserService {
+public class UserService implements CommunityConstant {
     @Autowired
     private UserMapper userMapper;
 
@@ -87,11 +88,23 @@ public class UserService {
         Context context = new Context(); // 设置邮箱要发送的内容
         context.setVariable("email", user.getEmail());
         // 拼url地址，http://localhost:8080/community/activation/101/code
-        String url = domain + context + "/activation/" + user.getId() + "/" + user.getActivationCode();
+        String url = domain + contextPath + "/activation/" + user.getId() + "/" + user.getActivationCode();
         context.setVariable("url", url);
         // 模板引擎自动识别context传入的参数，然后动态加载到网页中，将网页的动态变量进行替换，并将网页内容加载到content中
         String content = templateEngine.process("/mail/activation", context);
         mailClient.sendMail(user.getEmail(), "激活账号", content);
         return map;
+    }
+
+    public int activation(int userId, String code) {
+        User user = userMapper.selectById(userId);
+        if (user.getStatus() == 1) {
+            return ACTIVATION_REPEAT;
+        } else if (user.getActivationCode().equals(code)) {
+            userMapper.updateStatus(userId, 1);
+            return ACTIVATION_SUCCESS;
+        } else {
+            return ACTIVATION_FAILURE;
+        }
     }
 }
