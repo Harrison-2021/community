35acb51eb7a96b61651bdf5f3f89aa599706439f
liusheng <liushenggcs@163.com>
2021-09-22 20:21:39 +0800

    3.2_发布帖子，用json，并过滤敏感信息

---
+++ src/main/java/com/nowcoder/community/controller/DiscussController.java
@@ -0,0 +1,43 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+@Controller
+@RequestMapping("/discuss")
+public class DiscussController {
+    @Autowired
+    private HostHolder hostHolder;
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String addDiscussPost(String title, String content) {
+        User user = hostHolder.getUser();
+        if(user == null) {
+            return CommunityUtil.getJSONString(403, "你还没有登录哦!请登录后再发布!");
+        }
+
+        DiscussPost discussPost = new DiscussPost();
+        discussPost.setUserId(user.getId());
+        discussPost.setTitle(title);
+        discussPost.setContent(content);
+        discussPost.setCreateTime(new Date());
+        discussPostService.addDiscussPostRows(discussPost);
+
+        // 报错情况，将来统一处理
+        return CommunityUtil.getJSONString(0, "发布成功!");
+    }
+}
